8b1bb61597194e1a829d85fd90b8a75835578f91
teja <tejakumar5423@>
2024-04-18 17:17:43 +0530

    completed till nump python

---
+++ machine learning/matplotlip/assg1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+
+    'Age': [25, 30, 35],
+
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+df = pd.dataframe(data)
+print(df)
+
+
+
